99d76ca7a79f174b987c4ba43d45b31cff7be0f8
Kaanoba <81994097+Kaanoba@users.noreply.github.com>
2024-10-01 22:23:09 +0300

    Delete backend directory

--- backend/routes/todo.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-const {getAllTodo, deleteTodo, updateTodo, postTodo, getTodo} = require('../controllers/todo');
-
-router.route('/').get(getAllTodo).post(postTodo);
-router.route('/:id').get(getTodo).put(updateTodo).delete(deleteTodo);
-
-module.exports = router;
